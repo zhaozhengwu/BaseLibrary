f055c57ba10d0ece95ddd1ac34db1d0577ff2e96
zhaozhengwu <zhw.zhao@muheda.com>
2018-09-06 10:06:39 +0800

    Initial commit

---
+++ base_module/src/main/java/com/example/muheda/base_module/Utils/IntentToActivity.java
@@ -0,0 +1,15 @@
+package com.example.muheda.base_module.Utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Created by 13660 on 2018/9/6.
+ */
+
+public class IntentToActivity {
+    
+    public static void skipToActivity(Activity activity,Class<? extends Activity> cls){
+        activity.startActivity(new Intent(activity,cls));
+    }
+}

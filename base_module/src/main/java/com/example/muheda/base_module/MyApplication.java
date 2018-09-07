6c41b1f9775b9dd2cd179497bc8f6d11f7c6c434
muheda <yunwei@muheda.com>
2018-09-07 18:03:05 +0800

    der

---
+++ base_module/src/main/java/com/example/muheda/base_module/MyApplication.java
@@ -0,0 +1,26 @@
+package com.example.muheda.base_module;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.*;
+import com.alibaba.android.arouter.launcher.ARouter;
+
+/**
+ * Created by 13660 on 2018/9/7.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initRouter(this);
+    }
+
+    public static void initRouter(Application application) {
+        if (com.alibaba.android.arouter.BuildConfig.DEBUG) {
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(application);
+    }
+}

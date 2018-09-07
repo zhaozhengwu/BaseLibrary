3d8ce1de11a129bc557b6bc798dd4f7b1fecde17
muheda <yunwei@muheda.com>
2018-09-07 18:39:35 +0800

    dfg

--- base_module/src/main/java/com/example/muheda/base_module/MyApplication.java
+++ base_module/src/main/java/com/example/muheda/base_module/MyApplication.java
@@ -17,7 +17,7 @@ public class MyApplication extends Application {
     }
 
     public static void initRouter(Application application) {
-        if (com.alibaba.android.arouter.BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG) {
             ARouter.openLog();     // 打印日志
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
